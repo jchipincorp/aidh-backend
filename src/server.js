c01397148f930e5de8ef0ee10b44486ce8fe9f16
jchipincorp <jchipincorp@gmail.com>
2026-09-16 12:12:30 +0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -29,6 +29,13 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/ping', (req, res) => {
+  res.json({
+    ok: true,
+    message: 'PING WORKS'
+  });
+});
+
 // Health check
 app.get('/api/health', async (req, res) => {
   try {
