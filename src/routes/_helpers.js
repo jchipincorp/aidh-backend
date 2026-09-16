4b0a876244817b0de8b5b5e167cd8daf5eb0124e
jchipincorp <jchipincorp@gmail.com>
2026-09-16 12:56:53 +0500

    Delete src directory

--- src/routes/_helpers.js
+++
@@ -1,8 +0,0 @@
-// src/routes/_helpers.js
-/** Wraps an async controller so a rejected promise reaches Express's
- * error handler instead of crashing the process unhandled. */
-function asyncRoute(fn) {
-  return (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
-}
-
-module.exports = { asyncRoute };
